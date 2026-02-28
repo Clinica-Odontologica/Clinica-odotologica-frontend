157cfb4bbce2358235ca40610033a92a18b75740
joaoDevL <joao.software.dev@gmail.com>
2026-02-28 12:10:51 -0500

    feat:Defining our routes

---
+++ src/utils/routes.ts
@@ -0,0 +1,35 @@
+export const routes = {
+
+  // Public - Landing
+  recepcion: "/reception",
+  doctor: "/agenda",
+  admin: "/dashboard",
+
+  NotFound: "*",
+
+  // Public - Auth
+  login: "/",
+
+  //Admin Pages
+  dashboard: "/dashboard",
+  dashboard_profile: "/dashboard/profile",
+
+  // Users Management
+  dashboard_users: "/dashboard/usuarios",
+  dashboard_user: "/dashboard/usuario/:id",
+  dashboard_user_new: "/dashboard/usuario/nuevo",
+  dashboard_user_edit: "/dashboard/usuario/edit/:id",
+
+  // Doctors Management
+  dashboard_odontologos: "/dashboard/odontologos",
+  dashboard_odontologo: "/dashboard/odontologo/:id",
+  dashboard_odontologo_new: "/dashboard/odontologo/nuevo",
+  dashboard_odontologo_edit: "/dashboard/odontologo/edit/:id",
+  
+  // Services Management
+  dashboard_servicios: "/dashboard/servicios",
+  dashboard_servicio: "/dashboard/servicio/:id",
+  dashboard_servicio_new: "/dashboard/servicio/nuevo",
+  dashboard_servicio_edit: "/dashboard/servicio/edit/:id",
+
+};
